df8c3a3a2090da6d4ca8cc3043fb708970ddfee5
Sergei Matheson <smatheson@dius.com.au>
2017-12-05 17:11:08 +1100

    Drive items in equipment list from state

--- src/EquipmentList.js
+++ src/EquipmentList.js
@@ -6,14 +6,16 @@ import Item from './Item'
 export default class EquipmentList extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      itemNames: ['Glove', 'Boot', 'Hood', 'Pauldron']
+    }
   }
 
   render() {
-    return (
-      <div className="EquipmentList">
-        <Item name="Glove" />
-        <Item name="Boot" />
-      </div>
-    )
+    return <div className="EquipmentList">{this.renderItems()}</div>
+  }
+
+  renderItems() {
+    return this.state.itemNames.map(name => <Item key={name} name={name} />)
   }
 }
