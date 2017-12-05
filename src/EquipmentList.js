2eeb5123d98f573ef9cdc200fef40cf246707b33
Sergei Matheson <smatheson@dius.com.au>
2017-12-05 16:50:50 +1100

    Added state to equipment list

--- src/EquipmentList.js
+++ src/EquipmentList.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react'
 import './EquipmentList.css'
 
 export default class EquipmentList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { description: 'Equipment list for' }
+    this.frenchify = this.frenchify.bind(this)
+  }
+
   render() {
     return (
-      <div className="EquipmentList">Equipment List for {this.props.name}!</div>
+      <div className="EquipmentList">
+        {this.state.description} {this.props.name}!
+        <button onClick={this.frenchify}>Frenchify!</button>
+      </div>
     )
+  }
+
+  frenchify() {
+    this.setState({ description: "Liste d'équipement pour" })
   }
 }
