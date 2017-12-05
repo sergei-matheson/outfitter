0bc7cedad63c4e0bb88eaf1e080e0fd280a72544
Sergei Matheson <smatheson@dius.com.au>
2017-12-05 16:27:20 +1100

    Add placeholder equipment list and re-prettify

---
+++ src/EquipmentList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './EquipmentList.css'
+
+const EquipmentList = props => {
+  return <div className="EquipmentList">Equipment List for {props.name}!</div>
+}
+export default EquipmentList
