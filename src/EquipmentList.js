f30a3efcf19a464cfeb0a12bc2d7bb70263f85a5
Sergei Matheson <smatheson@dius.com.au>
2017-12-08 10:16:46 +1100

    Add loading screen

--- src/EquipmentList.js
+++ src/EquipmentList.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import { List } from 'semantic-ui-react'
-import { compose, withState, lifecycle, setDisplayName } from 'recompose'
+import { List, Dimmer, Loader } from 'semantic-ui-react'
+import {
+  compose,
+  withState,
+  lifecycle,
+  setDisplayName,
+  branch,
+  renderComponent
+} from 'recompose'
 import { map, pluck } from 'ramda'
 import './EquipmentList.css'
 import Item from './Item'
@@ -10,13 +17,25 @@ import { fetchItems } from './Client'
 export default compose(
   setDisplayName('EquipmentList'),
   withState('itemNames', 'updateItemNames', []),
+  withState('isLoading', 'updateIsLoading', false),
   lifecycle({
     async componentDidMount() {
+      const { updateIsLoading } = this.props
+      updateIsLoading(true)
       let items = await fetchItems()
       let { updateItemNames } = this.props
       updateItemNames(pluck('name', items))
+      updateIsLoading(false)
     }
-  })
+  }),
+  branch(
+    ({ isLoading }) => isLoading,
+    renderComponent(() => (
+      <Dimmer active>
+        <Loader>Loading</Loader>
+      </Dimmer>
+    ))
+  )
 )(({ itemNames }) => (
   <List className="EquipmentList">
     {map(
