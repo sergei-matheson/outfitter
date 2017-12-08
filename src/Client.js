8af9461504fc956448ff16e0148de65bfc5834a2
Sergei Matheson <smatheson@dius.com.au>
2017-12-08 15:48:20 +1100

    Test client with fetch-mock

--- src/Client.js
+++ src/Client.js
@@ -1,3 +1,5 @@
-export const fetchItems = () => [{ name: 'client wifflebat' }]
+const api_url = 'http://jsonplaceholder.typicode.com/comments'
+
+export const fetchItems = () => fetch(api_url).then(response => response.json())
 
 export default { fetchItems }
--- src/Client.test.js
+++ src/Client.test.js
@@ -1,11 +1,29 @@
 import { fetchItems } from './Client'
+import fetchMock from 'fetch-mock'
+
+const items = [
+  {
+    body: 'laudantium enim quasi est quidem magnam voluptate ipsam eos',
+    email: 'Eliseo@gardner.biz',
+    id: 1,
+    name: 'id labore ex et quam laborum',
+    postId: 1
+  },
+  {
+    body: 'voluptate iusto quis nobis reprehenderit ipsum amet nulla',
+    email: 'Carmen_Keeling@caroline.name',
+    id: 10,
+    name: 'eaque et deleniti atque tenetur ut quo ut',
+    postId: 2
+  }
+]
 
 describe('fetchItems', () => {
-  it('returns one hardwired item', () => {
-    try {
-      expect(fetchItems()).toEqual([{ name: 'client wifflebat' }])
-    } catch (e) {
-      done.fail(e)
-    }
+  it('returns items from the API as an array of objects', async () => {
+    fetchMock.get(
+      'http://jsonplaceholder.typicode.com/comments',
+      JSON.stringify(items)
+    )
+    return expect(await fetchItems()).toEqual(items)
   })
 })
