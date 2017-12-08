c95e4baf2e97c94e0c9988ebe705971321470705
Sergei Matheson <smatheson@dius.com.au>
2017-12-08 15:01:52 +1100

    Remove currently unused client code

--- src/Client.js
+++ src/Client.js
@@ -1,6 +1,3 @@
-const api_url = 'http://jsonplaceholder.typicode.com/comments'
-
-//export const fetchItems = () => fetch(api_url).then(response => response.json())
 export const fetchItems = () => [{ name: 'client wifflebat' }]
 
 export default { fetchItems }
