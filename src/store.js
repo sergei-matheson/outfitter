d1a9bd49582f3849c7af0c5b6f4969512e93cd89
Sergei Matheson <smatheson@dius.com.au>
2017-12-13 10:27:09 +1100

    Added redux dev tools, fixed store test

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
 import reducer from './reducers'
 import thunk from 'redux-thunk'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { createLogger } from 'redux-logger'
+
+const composeEnhancers = global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export default includeLogging => {
   const middleware = [thunk]
   if (includeLogging) {
     middleware.push(createLogger())
   }
-  return createStore(reducer, applyMiddleware(...middleware))
+  return createStore(reducer, composeEnhancers(applyMiddleware(...middleware)))
 }
--- src/store.test.js
+++ src/store.test.js
@@ -1,49 +1,57 @@
 jest.mock('./reducers', () => 'combined-reducer')
 
 import { createLogger } from 'redux-logger'
+
 jest.mock('redux-logger', () => ({
   createLogger() {
     return 'created-logger'
   }
 }))
+
 jest.mock('redux', () => {
   return {
     createStore: jest.fn(),
+    compose: jest.fn(() => 'composed-middleware'),
     applyMiddleware: jest.fn(() => 'applied-middleware')
   }
 })
 
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 
 import initStore from './store'
 
 import thunk from 'redux-thunk'
 
 describe('initialising the store', () => {
-  beforeEach(() => {
-    jest.clearAllMocks()
-  })
+  beforeEach(() => jest.clearAllMocks())
 
-  it('includes the combined reducer and applied middleware', () => {
-    initStore()
-    expect(createStore).toBeCalledWith('combined-reducer', 'applied-middleware')
-  })
+  describe('by default', () => {
+    beforeEach(() => initStore())
 
-  it('adds thunk middleware', () => {
-    initStore()
-    expect(applyMiddleware).toBeCalledWith(thunk)
+    it('composes the applied middleware', () => {
+      expect(compose).toBeCalledWith('applied-middleware')
+    })
+
+    it('includes the combined reducer and applied middleware', () => {
+      expect(createStore).toBeCalledWith(
+        'combined-reducer',
+        'composed-middleware'
+      )
+    })
   })
 
   describe('when logging is not included', () => {
-    it('does not add logging middleware', () => {
-      initStore(false)
+    beforeEach(() => initStore(false))
+
+    it('adds only thunk middleware', () => {
       expect(applyMiddleware).toBeCalledWith(thunk)
     })
   })
 
   describe('when logging is included', () => {
-    it('adds logging middleware', () => {
-      initStore(true)
+    beforeEach(() => initStore(true))
+
+    it('adds thunk and logging middleware', () => {
       expect(applyMiddleware).toBeCalledWith(thunk, 'created-logger')
     })
   })
