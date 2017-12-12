fcbc3b5eaf132dc8072373dbd909b23559f5a6fb
Sergei Matheson <smatheson@dius.com.au>
2017-12-13 10:52:41 +1100

    jsonplaceholder is currently down

--- src/Client.js
+++ src/Client.js
@@ -1,5 +1,28 @@
-const api_url = 'http://jsonplaceholder.typicode.com/comments'
+//const API_URL = 'http://jsonplaceholder.typicode.com/comments'
+//export const fetchItems = () => fetch(API_URL).then(response => response.json())
 
-export const fetchItems = () => fetch(api_url).then(response => response.json())
+const delayIt = value => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(value), 2000)
+  })
+}
+const items = [
+  {
+    body: 'laudantium enim quasi est quidem magnam voluptate ipsam eos',
+    email: 'Eliseo@gardner.biz',
+    id: 1,
+    name: 'id labore ex et quam laborum',
+    postId: 1
+  },
+  {
+    body: 'voluptate iusto quis nobis reprehenderit ipsum amet nulla',
+    email: 'Carmen_Keeling@caroline.name',
+    id: 10,
+    name: 'eaque et deleniti atque tenetur ut quo ut',
+    postId: 2
+  }
+]
+
+export const fetchItems = () => Promise.resolve(items).then(delayIt)
 
 export default { fetchItems }
